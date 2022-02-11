5512ad66417d2f6c8dd8b4b2639f3fa11f40fcce
EsmondKim <ekelectronicmail@gmail.com>
2022-02-11 11:47:20 -0600

    11:47 am 2.11.22

--- NavigatingInDBeaver.sql
+++ NavigatingInDBeaver.sql
@@ -5,5 +5,31 @@ select * from employees;
 
 select * from employees where employeenumber = 1102;
 
+-- This gives you the number of rows in orders table:
 select count(*) from orders;
+
+--This is how you commen a single line.
+/* 
+*This is how you comment multiple lines,
+*because it's longer than 1 line and,
+when you have a lot to say.
+*/
+
+select contactFirstName, customerNumber from customers;
+select contactFirstName as `First Name`, customerNumber as `Last Name` from customers;
+-- 
+-- for readability, it's good to split commands onto separate lines.
+select contactFirstName as `First Name`, customerNumber as `Last Name` 
+from customers
+order by contactfirstname;
+
+-- check out how count works for these different arguments:
+select * from offices;
+select count(*) from offices;
+select count(addressline2) from offices;
+select count(country) from offices;
+
+
+
+
 
